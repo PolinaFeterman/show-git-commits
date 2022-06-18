2390cc9dfc721d8bbcd9f547fe91b33429075342
Polina <polina.feterman@gmail.com>
2022-06-17 21:10:10 -0400

    add gitInfo script

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import generatedGitInfo from './generatedGitInfo.json';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="git-info">
+          <p>
+            <strong>Git Branch:</strong>{' '}
+            <code>{generatedGitInfo.gitBranch}</code>
+          </p>
+          <p>
+            <strong>Git Commit Hash:</strong>{' '}
+            <code>{generatedGitInfo.gitCommitHash}</code>
+          </p>
+        </div>
       </header>
     </div>
   );
